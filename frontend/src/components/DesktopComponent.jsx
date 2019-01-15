203a26fbfdd70e7eaaececeb7c1f033001310681
a.uvarov <sanyauvarov@mail.ru>
2019-01-15 10:56:02 +0300

    visual improvements

--- frontend/src/components/DesktopComponent.jsx
+++ frontend/src/components/DesktopComponent.jsx
@@ -3,13 +3,14 @@ import React from 'react';
 import TableContainer from '../containers/TableContainer.jsx';
 
 const DesktopComponent = () => (
-    <div className="desktop-div">
-        <div className="desktop-table-div">
-            {'Составление'}
+    <div className="desktop-block">
+        <div className="desktop-block__block">
+            <h3 className="desktop-block__header">{'Составление'}</h3>
             <TableContainer mode={1} />
         </div>
-        <div className="desktop-table-div">
-            {'Покупка'}
+
+        <div className="desktop-block__block">
+            <h3 className="desktop-block__header">{'Покупка'}</h3>
             <TableContainer mode={2} />
         </div>
     </div>
